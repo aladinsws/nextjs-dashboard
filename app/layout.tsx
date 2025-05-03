a8936aca25958fa69c2a78a3ee7c12148b153dda
aladinsws <aladinsws@gmail.com>
2025-05-03 19:09:15 +0200

    Chapter 16: Adding Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '%s | Acme dashboard',
+  description: 'The official Next.js Course Dashboard , built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-aladinsws-projects.vercel.app/'),
+}
 
 export default function RootLayout({
   children,
